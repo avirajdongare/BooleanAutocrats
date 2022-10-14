015caf04529e9f2bb75c66062b831052926c51a4
DHRUV SHARMA <115227371+dhruvsharma2001@users.noreply.github.com>
2022-10-14 23:18:31 +0530

    Delete JAVA programming language directory

--- JAVA programming language/dowhile.java
+++
@@ -1,9 +0,0 @@
-public class dowhile {
-    public static void main(String args[]){
-        int counter = 1;
-        do{
-            System.out.println("bhanu");
-            counter++;
-        }while(counter<10);
-}
-}
