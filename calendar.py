addbb98f56dfe34531c8b428692d70a2ca7d3180
Minh Le <ledoannhatminh31@gmail.com>
2022-11-08 09:08:54 +0700

    coin-flipping game

--- calendar.py
+++
@@ -1,4 +0,0 @@
-import calendar
-yy = 2022  
-mm = 10  
-print(calendar.month(yy, mm))
--- class.py
+++
@@ -1,7 +0,0 @@
-class Employee:    
-    id = 19   
-    name = "Gauri"    
-    def display (self):    
-        print("ID: %d \nName: %s"%(self.id,self.name))    
-emp = Employee()    
-emp.display()   
