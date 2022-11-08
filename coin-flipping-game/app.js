b4a1749bc0bb8117270f1a58c60995f7cabf633f
Minh Le <ledoannhatminh31@gmail.com>
2022-11-08 10:03:09 +0700

    coin-flipping game update

--- coin-flipping-game/app.js
+++ coin-flipping-game/app.js
@@ -5,36 +5,38 @@ let flipBtn = document.querySelector("#flip-button");
 let resetBtn = document.querySelector("#reset-button");
 
 flipBtn.addEventListener("click", () => {
-    let i = Math.floor(Math.random() * 2);
-    coin.style.animation = "none";
-    if(i){
-        setTimeout(function(){
-            coin.style.animation = "spin-heads 3s forwards";
-        }, 100);
-        heads++;
-    }
-    else{
-        setTimeout(function(){
-            coin.style.animation = "spin-tails 3s forwards";
-        }, 100);
-        tails++;
-    }
-    setTimeout(updateStats, 3000);
-    disableButton();
+  // random number 0 or 1
+  let i = Math.floor(Math.random() * 2);
+  coin.style.animation = "none";
+  if (i) {
+    // if i == 0, its tail, otherwise its head
+    setTimeout(function () {
+      coin.style.animation = "spin-heads 3s forwards";
+    }, 100);
+    heads++;
+  } else {
+    setTimeout(function () {
+      coin.style.animation = "spin-tails 3s forwards";
+    }, 100);
+    tails++;
+  }
+  setTimeout(updateStats, 3000);
+  disableButton();
 });
-function updateStats(){
-    document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
-    document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
+
+function updateStats() {
+  document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
+  document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
 }
-function disableButton(){
-    flipBtn.disabled = true;
-    setTimeout(function(){
-        flipBtn.disabled = false;
-    },3000);
+function disableButton() {
+  flipBtn.disabled = true;
+  setTimeout(function () {
+    flipBtn.disabled = false;
+  }, 3000);
 }
-resetBtn.addEventListener("click",() => {
-    coin.style.animation = "none";
-    heads = 0;
-    tails = 0;
-    updateStats();
+resetBtn.addEventListener("click", () => {
+  coin.style.animation = "none";
+  heads = 0;
+  tails = 0;
+  updateStats();
 });
